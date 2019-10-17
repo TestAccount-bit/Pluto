47120c14ccfb25d2a1155b473038cc54d1c1f2ec
TestAccount-bit <56519912+TestAccount-bit@users.noreply.github.com>
2019-10-17 21:36:05 +0200

    Update Calculation.java

--- SimpleCalculating/src/Calculation.java
+++ SimpleCalculating/src/Calculation.java
@@ -5,6 +5,16 @@ public class Calculation {
 
 	public static void main(String[] args) {
 		
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Program outups all prime numbers, smaller than x.");
+		System.out.println("Enter x:");
+		int n = input.nextInt();
+		int max = floorSqrt(n);
+		
+		boolean[] primeNumbers = new boolean[n];
+		for(int i=0;i<n;i++) {
+			primeNumbers[i] = true;
 		}
 		
 		for(int i=2;i<max;i++) {
